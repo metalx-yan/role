eb3c2eb48d88fb72bfb49db00d4969c219382b9c
Aldian Firmansyah <farandibagas@gmail.com>
2018-09-13 09:04:25 +0700

    role

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Auth;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+
+    protected $redirectTo;
+
+    protected $username = 'username';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+
+    public function redirectTo(){
+
+        // User role
+        $role = Auth::user()->roles()->whereIn('name', ['admin','superadmin'])->first()->name;
+
+        // Check user role
+        switch ($role) {
+            case 'admin':
+                return '/admin';
+                break;
+            case 'superadmin':
+                return '/superadmin';
+                break;
+            default:
+                return '/login';
+                break;
+        }
+    }
+    /**
+     * @return string
+     */
+    /**
+     * @return string
+     */
+    
+}
